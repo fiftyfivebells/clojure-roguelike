82485422e1fcac8b8f5e63e08a728d896c59d992
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2026-06-29 11:27:10 -0400

    Add messages and prompts

--- src/roguelike/update.clj
+++ src/roguelike/update.clj
@@ -3,7 +3,20 @@
 
 (defn update-world
   [world input]
-  (case (:type input)
-    :move (world/move-player world {:x (:dx input) :y (:dy input)})
-    :quit (assoc world :mode :quit)
-    :none world))
+  (let [world (assoc world :current-msg "")]
+    (case (:type input)
+      :move (let [delta {:x (:dx input) :y (:dy input)}
+                  moved (world/move-player world delta)
+                  collision? (= (:player moved) (:player world))]
+              (cond-> moved
+                collision? (world/add-message "You ran into a wall.")))
+
+      :prompt (world/set-mode world {:screen :prompt
+                                     :message (:message input)
+                                     :on-yes (:on-yes input)
+                                     :return (:return input)})
+
+      :return (world/set-mode world (:return (:mode world)))
+
+      :quit (world/set-mode world :quit)
+      :none world)))
--- src/roguelike/world.clj
+++ src/roguelike/world.clj
@@ -2,15 +2,36 @@
 
 (defn new-world
   []
-  {:player {:x 10 :y 12}
-   :width 80
-   :height 24
-   :mode :play})
+   {:player {:x 10 :y 12}
+    :width 80
+    :play-start-row 1
+    :map-rows 22
+    :msg-row 23
+    :current-msg "Welcome to the dungeon."
+    :messages ["Welcome to the dungeon."]
+    :height 24
+    :mode {:screen :play}})
 
 (defn clamp-position
   [world {new-x :x new-y :y}]
   {:x (max 0 (min new-x (- (:width world) 1)))
-   :y (max 0 (min new-y (- (:height world) 1)))})
+   :y (max 0 (min new-y (- (:map-rows world) 1)))})
+
+(defn add-message
+  [world message]
+  (-> world
+      (assoc :current-msg message)
+      (update :messages conj message)))
+
+(defn get-walking-message
+  [delta]
+  (letfn [(find-direction [{dx :x dy :y}]
+            (case [dx dy]
+              [0 -1] "north"
+              [0 1] "south"
+              [1 0] "east"
+              [-1 0] "west"))]
+    (str "You take a step " (find-direction delta) ".")))
 
 (defn move-player
   [world delta]
@@ -18,4 +39,8 @@
         new-x (+ (:x player-pos) (:x delta))
         new-y (+ (:y player-pos) (:y delta))
         new-pos (clamp-position world {:x new-x :y new-y})]
-    (assoc-in world [:player] new-pos)))
+    (assoc world :player new-pos)))
+
+(defn set-mode
+  [world mode]
+  (assoc world :mode mode))
