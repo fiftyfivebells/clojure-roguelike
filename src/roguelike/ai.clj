5ce2fc72fcb29412e95c0cfd5508c708d27958ca
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2026-07-15 22:32:53 -0400

    Add ai namespace and random walk behavior

---
+++ src/roguelike/ai.clj
@@ -0,0 +1,15 @@
+(ns roguelike.ai
+  [:require [roguelike.rng :as rng]])
+
+(def ^:private directions
+  [[-1 -1] [0 -1] [1 -1]
+   [-1  0]        [1  0]
+   [-1  1] [0  1] [1  1]])
+
+(defn decide
+  [rng-state world actor-id]
+  (let [[rng-state val] (rng/rand-int-range rng-state 0 (inc (count directions)))
+        action (if (>= val (count directions))
+                 {:type :world/wait}
+                 {:type :world/move :delta (nth directions val)})]
+    [rng-state action]))
