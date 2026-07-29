b54975c12304c1d4ee4de0eea3f98301ff448203
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2026-07-29 13:57:31 -0400

    Replace step-toward with chase (smarter following behavior)

--- src/roguelike/ai.clj
+++ src/roguelike/ai.clj
@@ -1,16 +1,13 @@
 (ns roguelike.ai
   (:require [clojure.math :as math]
             [roguelike.rng :as rng]
-            [roguelike.world :as world]))
+            [roguelike.world :as world]
+            [roguelike.pathfinding :as pathfinding]))
 
 (def ^:private directions
   [[-1 -1] [0 -1] [1 -1]
    [-1  0]        [1  0]
    [-1  1] [0  1] [1  1]])
-
-(defn- step-toward
-  [[mx my] [px py]]
-  [(long (math/signum (- px mx))) (long (math/signum (- py my)))])
 
 (defn- wander
   [world]
@@ -20,13 +17,24 @@
     [world action]))
 
 (defn- chase
-  [world monster player-pos]
-  [world {:action/type :world/move :delta (step-toward (:pos monster) player-pos)}])
+  [world monster]
+  (let [walkable? (partial world/walkable-at? world)
+        field (pathfinding/distance-map walkable? (world/player-pos world))
+        monster-pos (:pos monster)
+        here-dist (field monster-pos)
+        neighbors (pathfinding/neighbors-in-map field monster-pos)]
+    (if (empty? neighbors)
+      (wander world)
+      (let [smallest (apply min-key val neighbors)]
+        (if (and here-dist (< (val smallest) here-dist))
+          (let [delta (mapv - (key smallest) monster-pos)]
+            [world {:action/type :world/move :delta delta}])
+          (wander world))))))
 
 (defn decide
   [world actor-id]
   (let [monster    (world/get-actor world actor-id)
         player-pos (world/player-pos world)]
     (if (world/can-see? world actor-id player-pos)
-      (chase world monster player-pos)
+      (chase world monster)
       (wander world))))
